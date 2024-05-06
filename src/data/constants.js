e4769fc1d00857abde729790bb2b75476f8b014d
Hemraj Choudhary <rudraman@MacBookPro14.local>
2024-05-06 19:39:10 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/rudraman7",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1jdIRtm_0mmVSryUX0T0ylLoIUahHa5CH/view?usp=share_link",
   linkedin: "https://www.linkedin.com/in/hemraj-choudhary/",
   twitter: "https://twitter.com/rudraman7",
   // insta: "https://www.instagram.com/rishav_chanda/",
